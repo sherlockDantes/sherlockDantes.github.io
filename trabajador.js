8545b46b09a0764d8d742291b880ac172a7069a9
sherlockDantes <96788518+sherlockDantes@users.noreply.github.com>
2022-01-07 16:12:29 -0500

    Delete trabajador.js

--- trabajador.js
+++
@@ -1,7 +0,0 @@
-function recibirMensaje(e) {
-    let respuesta = "El texto escrito en el cuadro es " + e.data;
-
-    postMessage(respuesta);
-}
-
-addEventListener("message", recibirMensaje, false);
